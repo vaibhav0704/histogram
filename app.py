b41c270126570159e1a36b75ce1277548a0a87db
vaibhav <vaibhavsaraf.08@gmail.com>
2023-11-17 13:48:03 +0530

    addded clahe

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import numpy as np
 import io
 from flask_cors import cross_origin
 from enhance_image import histogram_equalization
+from clahe import clahe
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 app = Flask(__name__)
 
-@app.route("/enhance", methods=["POST"])
+@app.route("/cdf", methods=["POST"])
 @cross_origin(origins="*")
 def enhance():
     image_file = request.files["image"]
@@ -25,5 +28,44 @@ def enhance():
         download_name='enhanced_image.png'
     )
 
+@app.route("/clahe", methods=["POST"])
+@cross_origin(origins="*")
+def clahe():
+    image_file = request.files["image"]
+    image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+
+    clahe_img = clahe(image[:,:,0],8,0,0)
+
+    def plot_to_image(image, clahe_img):
+        fig, axs = plt.subplots(1, 2, figsize=(200, 100))
+        axs[0].imshow(image[:, :, 0], cmap='gray')
+        axs[1].imshow(clahe_img, cmap='gray')
+
+        # Create a canvas to render the figure to an image
+        canvas = FigureCanvasAgg(fig)
+        canvas.draw()
+
+        # Convert the plot to an image and return it
+        width, height = fig.get_size_inches() * fig.get_dpi()
+        image = np.frombuffer(canvas.tostring_rgb(), dtype='uint8').reshape(int(height), int(width), 3)
+
+        # Close the plot to release resources
+        plt.close(fig)
+        plt.show()
+
+        return image
+
+    plot_image = plot_to_image(image, clahe_img)
+
+    _, img_encoded = cv2.imencode('.png', plot_image)
+    img_bytes = img_encoded.tobytes()
+
+    return send_file(
+        io.BytesIO(img_bytes),
+        mimetype='image/png',
+        as_attachment=True,
+        download_name='enhanced_image.png'
+    )
+    
 if __name__ == "__main__":
     app.run(debug=True)
